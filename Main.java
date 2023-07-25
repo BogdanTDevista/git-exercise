4e0ec5dd1f8305dafb0be88429dbb20cd106be6f
BogdanTDevista <140379672+BogdanTDevista@users.noreply.github.com>
2023-07-25 23:14:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(add(2, 4));
+    }
+    public static int add(int a, int b){
+        return a + b;
+    }
+}
