f239513662ce137ab1fe92faaa2c21f110b18f58
Anastasia Smarandi <anastasiasmarandi@Anastasias-MacBook-Pro.local>
2023-07-26 10:22:26 +0300

    Created add4 function

--- Main.java
+++ Main.java
@@ -7,4 +7,8 @@ public class Main {
     public static int add3(int a, int b, int c){
         return a + b + c;
     }
+
+    public static int add4(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
 }
